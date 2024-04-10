5f48b5d4fff40a2f82e525ea2c802943a3002f3b
ViniciusBoroto <viniciusdosanjos63@gmail.com>
2024-04-09 21:58:17 -0300

    Adição do cone e sua movimentação de teste

--- Program.py
+++ Program.py
@@ -10,10 +10,11 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 pygame.display.set_caption('Mathematic Park')
 
-player_surface = pygame.image.load('img/player200.png')
+player_surface = pygame.image.load('img/player200.png').convert_alpha()
+scene_surface = pygame.image.load('img/scene.jpg').convert()
 
-
-scene_surface = pygame.image.load('img/scene.jpg')
+cone_surface = pygame.image.load('img/cone.png').convert_alpha()
+cone_xpos = 1280
 
 while True:
     for event in pygame.event.get():
@@ -21,8 +22,16 @@ while True:
             pygame.quit()
             exit()
 
+
+
     screen.blit(scene_surface, (0, -7))
     screen.blit(player_surface, (80, 356))
+
+    cone_xpos -= 8
+    if cone_xpos < -100:
+        cone_xpos = 1280
+
+    screen.blit(cone_surface, (cone_xpos, 450))
 
     pygame.display.update()
     clock.tick(30)
