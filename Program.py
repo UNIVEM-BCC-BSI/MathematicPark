8983666cf5165f6f998773331d9d3154833d502f
ViniciusBoroto <viniciusdosanjos63@gmail.com>
2024-04-10 21:34:25 -0300

    Chão adicionado

--- Program.py
+++ Program.py
@@ -6,6 +6,8 @@ pygame.init()
 # Constantes
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+GROUND_LEVEL = 555
+JUMP_VALUE = -25
 
 #Variáveis do Pygame
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -14,15 +16,15 @@ pygame.display.set_caption('Mathematic Park')
 
 #Variáveis do player
 player_surface = pygame.image.load('img/player200.png').convert_alpha()
-player_rect = player_surface.get_rect(midbottom = (160, 555))
-player_gravity = -25
+player_rect = player_surface.get_rect(midbottom = (160, GROUND_LEVEL))
+player_gravity = 0
 
 #Variáveis do cenário
 scene_surface = pygame.image.load('img/scene.jpg').convert()
 
 #Variáveis do obstáculo
 cone_surface = pygame.image.load('img/cone.png').convert_alpha()
-cone_rect = cone_surface.get_rect(midbottom = (1330, 550))
+cone_rect = cone_surface.get_rect(midbottom = (1330, GROUND_LEVEL))
 
 #Loop do jogo
 while True:
@@ -36,25 +38,29 @@ while True:
         #Eventos de pulo
         if event.type == pygame.MOUSEBUTTONDOWN:
             if player_rect.collidepoint(event.pos):
-                player_gravity = -20
+                player_gravity = JUMP_VALUE
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE or event.key == pygame.K_UP or event.key == pygame.K_PAGEUP: 
-                player_gravity = -20
+                player_gravity = JUMP_VALUE
 
     #Funcionalidade da gravidade
     player_gravity += 2
     player_rect.y += player_gravity
 
+    #Funcionalidade do chão
+    if player_rect.bottom > GROUND_LEVEL:
+        player_rect.bottom = GROUND_LEVEL
+
     #Mecânica de teste do movimento do cone
-    cone_rect.x -= 8
+    cone_rect.x -= 12
     if cone_rect.right <= 0:
         cone_rect.left = 1280
 
     #Atualizações no display
     screen.blit(scene_surface, (0, -7))
-    screen.blit(player_surface, player_rect)
     screen.blit(cone_surface, cone_rect)
+    screen.blit(player_surface, player_rect)
 
 
 
